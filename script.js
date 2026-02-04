62c231986cf0771524fb0c3b858e57bf010f1f84
_DarknessXD <darknessxd0029@gmail.com>
2026-02-04 23:45:17 +0530

    script.js

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ function createHeart() {
 }
 
 setInterval(createHeart, 250);
+let boyPos = 10;
+let solved = false;
+
+function startSurprise() {
+  document.getElementById("message").classList.remove("hidden");
+  document.getElementById("game").classList.remove("hidden");
+  document.getElementById("bg-music").play();
+}
+
+document.addEventListener("keydown", (e) => {
+  if (solved) return;
+
+  const boy = document.getElementById("boy");
+
+  if (e.key === "ArrowRight") boyPos += 10;
+  if (e.key === "ArrowLeft") boyPos -= 10;
+
+  boy.style.left = boyPos + "px";
+
+  if (boyPos > 160) {
+    solved = true;
+    document.getElementById("game").classList.add("hidden");
+    document.getElementById("reward").classList.remove("hidden");
+  }
+});
+
